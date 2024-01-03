fa678dc0db88e71e3f41a264e909b7ea0dd230ac
qmer05 <cph-oo203@cphbusiness.dk>
2024-01-03 22:06:48 +0100

    update

--- src/main/java/_8_Stop_madspild/FoodWaste.java
+++ src/main/java/_8_Stop_madspild/FoodWaste.java
@@ -21,6 +21,6 @@ public class FoodWaste {
     public boolean checkDate(int category, LocalDate saleByDate) {
         int limitValue = limits.get(category);
         LocalDate stopFoodWasteDate = saleByDate.minusDays(limitValue + 1); // +1 for at korigere indeks
-        return stopFoodWasteDate.isBefore(todaysDate) && saleByDate.isAfter(todaysDate);
+        return stopFoodWasteDate.isBefore(todaysDate) && saleByDate.isAfter(todaysDate) || saleByDate.equals(todaysDate);
     }
 }
